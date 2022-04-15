617a42482f68e393dccd25ad4bc0707ff796844d
Jhr0576 <jacob.rice@kellogg.northwestern.edu>
2022-04-15 20:56:28 +0000

    3:56

--- crm/crm.sql
+++ crm/crm.sql
@@ -6,6 +6,6 @@
 
 Create Table contacts (id Integer Primary Key autoincrement, first_name TEXT, last_name TEXT, email TEXT, phone_number, company_id TEXT);
 Create Table companies (id Integer Primary Key autoincrement, name TEXT);
-Create Table activities (id Integer Primary Key autoincrement, call TEXT, email TEXT, date TEXT, time TEXT);
+Create Table activities (id Integer Primary Key autoincrement, created_at TEXT, notes TEXT, salesperson_id integer, contact_id integer);
 Create Table personal_info (id Integer Primary Key autoincrement, first_name TEXT, last_name TEXT, email TEXT);
 See solution, this isn't correct 
